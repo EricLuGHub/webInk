6b8c801e1747bdc966e756d49826d4e858b7a162
ericlu0306 <erickandmorty2013@gmail.com>
2025-08-18 14:50:53 -0400

    chore: typo

--- backend/prompt_router.py
+++ backend/prompt_router.py
@@ -8,4 +8,4 @@ prompt_router = APIRouter(prefix="", tags=[])
 
 @prompt_router.post("/prompt")
 async def create_badge(completion_prompt : ConnectorAuthorizeRequest, svc:  PromptService = Depends(get_prompt_service)):
-    svc.create_completion(ConnectorAuthorizeRequest)
+    svc.create_completion(completion_prompt)
